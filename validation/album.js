6189b898fe0be090e0a4728d6f5817267ef15eaa
Emma Käck <emma.keck@hotmail.com>
2022-03-05 13:19:06 +0100

    validation rules on POST new album

--- validation/album.js
+++ validation/album.js
@@ -6,9 +6,19 @@
 // conbined from app.js
 const { body } = require('express-validator');
 const models = require('../models');
+const Album = require('../models/Album');
+const User = require('../models/User');
 
 const toCreate_rules = [ 
-   body('title').exists().isLength({ min: 4 })
+   body('title').exists().isLength({ min: 4 }),
+   body('user_id').exists().bail().custom(async value => {
+    const user = await new models.User({ id: value }).fetch({ require: false });
+    if (!user) {
+        return Promise.reject(`User with ID ${value} does not exist.`);
+    }
+
+    return Promise.resolve();
+}),
 ];
 
 const toUpdate_rules = [
