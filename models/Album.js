5229274164bcc7cd2e05bcea95921a4bdd99f952
Emma Käck <emma.keck@hotmail.com>
2022-04-01 18:15:58 +0200

    changes place on user and photo

--- models/Album.js
+++ models/Album.js
@@ -2,12 +2,12 @@
 module.exports = (bookshelf) => {
 	return bookshelf.model('Album', {
 		tableName: 'albums',
-		users() {
-			return this.belongsTo('User');
-		},
 		photos() {
 			//?belongstomany | hasMany
 			return this.belongsToMany('Photo');
+		},
+		users() {
+			return this.belongsTo('User');
 		}
 	});
 }
