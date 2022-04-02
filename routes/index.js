0bcb5d02702c36b3e893a2d62cc200ff980c0b29
Emma Käck <emma.keck@hotmail.com>
2022-04-02 15:41:47 +0200

    Deleted profile

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,22 @@
  * INDEX ROUTER
  * 
  */
-const auth =require('../middlewares/auth');
-const express = require('express');
-const router = express.Router();
-const validation_rules_forUser = require('../validation/user');
-const registerController = require('../controllers/register_controller')
-
-/* GET / */
-router.get('/', (req, res, next) => {
-	res.send({ success: true, data: { msg: 'oh, hi' }});
-});
-
-
-router.use('/albums', require('./albums'));
-router.use('/photos', require('./photos'));
-router.use('/profile', auth.basic, require('./profile'));
-
-router.post('/register', validation_rules_forUser.toCreate_rules, registerController.register);
-
-module.exports = router;
+ const auth =require('../middlewares/auth');
+ const express = require('express');
+ const router = express.Router();
+ const validation_rules_forUser = require('../validation/user');
+ const registerController = require('../controllers/register_controller')
+ 
+ /* GET / */
+ router.get('/', (req, res, next) => {
+	 res.send({ success: true, data: { msg: 'oh, hi' }});
+ });
+ 
+ 
+ router.use('/albums', require('./albums'));
+ router.use('/photos', require('./photos'));
+ router.use('/profile', auth.basic, require('./profile'));
+ 
+ router.post('/register', validation_rules_forUser.toCreate_rules, registerController.register);
+ 
+ module.exports = router;
