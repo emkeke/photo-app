5b13d36eae663f20b6cf5a4de0b0be3f45436420
Emma Käck <emma.keck@hotmail.com>
2022-04-02 15:44:05 +0200

    Delelted profile validation

---
+++ validation/photo.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * Validation rules for photos
+ */
+
+
+ const { urlencoded } = require('express');
+ const { body } = require('express-validator');
+ 
+ 
+ const toCreate_rules = [ 
+    body('title').exists().isLength({ min: 4 }),
+    body('url').exists().isURL(),
+    body('comment').optional().isLength({ min: 4 }),
+ ];
+ 
+ const toUpdate_rules = [
+     body('title').optional().isLength({ min: 4 }),
+     body('url').optional().isURL(),
+     body('comment').optional().isLength({ min: 4 }),
+ ];
+ 
+ 
+ module.exports = {
+     toCreate_rules,
+     toUpdate_rules,
+ }
--- validation/user.js
+++ validation/user.js
@@ -2,29 +2,29 @@
  * 
  * Validation rules for user
  */
-const { body } = require('express-validator');
+ const { body } = require('express-validator');
 
-const toCreate_rules = [ 
-    body('email').exists().isEmail().custom(async value => {
-        const email = await new models.User({ email: value }).fetch({ require: false });
-        if (email) {
-            return Promise.reject('A user with this email already exists');
-        }
-        return Promise.resolve();
-    }),
-    body('password').exists().isLength({ min: 6 }),
-    body('first_name').exists().isLength({ min: 2 }),
-    body('last_name').exists().isLength({ min: 2 }),
+ const toCreate_rules = [ 
+     body('email').exists().isEmail().custom(async value => {
+         const email = await new models.User({ email: value }).fetch({ require: false });
+         if (email) {
+             return Promise.reject('A user with this email already exists');
+         }
+         return Promise.resolve();
+     }),
+     body('password').exists().isLength({ min: 6 }),
+     body('first_name').exists().isLength({ min: 2 }),
+     body('last_name').exists().isLength({ min: 2 }),
+  ];
+ 
+ const toUpdate_rules = [
+     body('password').optional().isLength({ min: 6 }),
+     body('first_name').optional().isLength({ min: 2 }),
+     body('last_name').optional().isLength({ min: 2 }),
  ];
-
-const toUpdate_rules = [
-    body('password').optional().isLength({ min: 6 }),
-    body('first_name').optional().isLength({ min: 2 }),
-    body('last_name').optional().isLength({ min: 2 }),
-];
-
-
-module.exports = {
-    toCreate_rules,
-    toUpdate_rules,
-}
+ 
+ 
+ module.exports = {
+     toCreate_rules,
+     toUpdate_rules,
+ }
