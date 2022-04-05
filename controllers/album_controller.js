4969956f986243d92631265305a7def7795b1890
Emma Käck <emma.keck@hotmail.com>
2022-04-05 19:30:03 +0200

    Fixed the addPhoto section

--- controllers/album_controller.js
+++ controllers/album_controller.js
@@ -132,6 +132,7 @@ const update = async (req, res) => {
 }
 
 const addPhoto = async (req, res) => {
+
     const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		return res.status(422).send({ status: 'fail', data: errors.array() });
@@ -139,16 +140,39 @@ const addPhoto = async (req, res) => {
 
     const validData = matchedData(req);
 
-    const user = await models.User.fetchById(req.user.id, { withRelated: ['albums'] });
+    const user = await models.User.fetchById(req.user.id, { withRelated: ['albums', 'photos'] });
 
-    const userAlbum = user.related('albums');
+    const userAlbum = user.related('albums')
+        .find(album => album.id == req.params.albumId);
 
-    const album = userAlbum.find(album => album.id == req.params.albumId);
-
-    if (!album) {
+    if (!userAlbum) {
         return res.status(404).send({
             status: 'fail',
             data: "Album could not be found",
+        });
+    }
+
+    const userPhoto = user.related('photos')
+        .find(photo => photo.id == validData.photo_id);
+
+    if (!userPhoto) {
+        return res.status(404).send({
+            status: 'fail',
+            data: "Photo could not be found",
+        });
+    }
+
+    const album = await models.Album.fetchById(req.params.albumId, { withRelated: ['photos']});
+
+    const albumPhotos = album.related('photos')
+        .find(photo => photo.id == validData.photo_id);
+
+    debug('Updated book successfully: %O', userPhoto);
+
+    if (albumPhotos) {
+        return res.status(409).send({
+            status: 'fail',
+            data: 'Photo already exists.',
         });
     }
 
